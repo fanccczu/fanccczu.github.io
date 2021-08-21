a0d1c9b5f8ee4cca90caf4f5c79ffed959a37925
fancczu <86477929+fancczu@users.noreply.github.com>
2021-08-21 21:03:26 +0800

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -10,6 +10,18 @@ function Reset() {
 	});
 }
 
+function getQueryVariable(url, variable) {
+	var query = url.split("?")[1];
+	var vars = query.split("&");
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split("=");
+		if (pair[0] == variable) {
+			return pair[1];
+		}
+	}
+	return (false);
+}
+
 var data_index = "1";
 var list = document.querySelector('.mui-table-view.mui-table-view-radio');
 list.addEventListener('selected', function(e) {
@@ -19,13 +31,19 @@ list.addEventListener('selected', function(e) {
 function Query() {
 	var input_data = document.getElementById("val").value;
 	if (input_data) {
+		if (input_data.split(".")[0] == "https://m" && input_data.split(".")[1] == "v" && input_data.split(".")[2] ==
+			"qq" && input_data.split(".")[3].split("/")[0] == "com") {
+			input_data = "https://v.qq.com/x/cover/" + getQueryVariable(input_data, "cid") + "/" + getQueryVariable(
+				input_data, "vid") + ".html";
+			console.log(input_data);
+		}
 		switch (data_index) {
 			case "1":
 				document.getElementById("card").style.display = "block";
 				document.getElementById("ifrm").src = "https://okjx.cc/?url=" + input_data;
 				break;
 			case "2":
-				document.getElementById("card").style.display = "block";	
+				document.getElementById("card").style.display = "block";
 				document.getElementById("ifrm").src = "https://jx.quanmingjiexi.com/?url=" + input_data;
 				break;
 			case "3":
